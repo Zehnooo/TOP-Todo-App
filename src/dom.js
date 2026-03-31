da2378499db8abad0afd1d4caeb846f7e803c3f0
Zehno <126801501+Zehnooo@users.noreply.github.com>
2026-03-31 16:35:01 -0500

    added/updated svgs to get long links out of JS file

--- src/dom.js
+++ src/dom.js
@@ -17,6 +17,9 @@ import completesvg from './assets/complete.svg';
 import copysvg from './assets/copy.svg';
 import deletesvg from './assets/delete.svg';
 import editsvg from './assets/edit.svg';
+import plussvg from './assets/plus.svg';
+import savesvg from './assets/save.svg';
+import homesvg from './assets/home.svg';
 
 
 export const buildDom = (() => {
@@ -58,7 +61,7 @@ function buildDefaultSidebar(){
         list.append(placeholder);
     }
         const home = document.createElement('button');
-        home.innerHTML = `<svg width="48px" height="48px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 9.77806V16.2C19 17.8801 19 18.7202 18.673 19.3619C18.3854 19.9264 17.9265 20.3854 17.362 20.673C17.2111 20.7499 17.0492 20.8087 16.868 20.8537M5 9.7774V16.2C5 17.8801 5 18.7202 5.32698 19.3619C5.6146 19.9264 6.07354 20.3854 6.63803 20.673C6.78894 20.7499 6.95082 20.8087 7.13202 20.8537M21 12L15.5668 5.96393C14.3311 4.59116 13.7133 3.90478 12.9856 3.65138C12.3466 3.42882 11.651 3.42887 11.0119 3.65153C10.2843 3.90503 9.66661 4.59151 8.43114 5.96446L3 12M7.13202 20.8537C7.65017 18.6447 9.63301 17 12 17C14.367 17 16.3498 18.6447 16.868 20.8537M7.13202 20.8537C7.72133 21 8.51495 21 9.8 21H14.2C15.485 21 16.2787 21 16.868 20.8537M14 12C14 13.1045 13.1046 14 12 14C10.8954 14 10 13.1045 10 12C10 10.8954 10.8954 9.99996 12 9.99996C13.1046 9.99996 14 10.8954 14 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`;
+        home.innerHTML = homesvg;
         home.addEventListener('click', () => { goHome(); setSelectedItem('home'); });
         home.id = 'home';
         home.classList.add('selected', 'btn', 'home-btn');
@@ -100,11 +103,7 @@ function buildDefaultDash(){
         newBtn.addEventListener('click', () => handleNewProjectForm());
 
     const plus = document.createElement('span');
-    plus.innerHTML = `<svg width="18px" height="18px" viewBox="0 -0.5 21 21"  xmlns="http://www.w3.org/2000/svg" fill="#000000">
-<g id="SVGRepo_bgCarrier" stroke-width="0"/>
-<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-<g id="SVGRepo_iconCarrier"> <title>plus_circle [#1427]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-179.000000, -600.000000)" fill="currentColor"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M137.7,450 C137.7,450.552 137.2296,451 136.65,451 L134.55,451 L134.55,453 C134.55,453.552 134.0796,454 133.5,454 C132.9204,454 132.45,453.552 132.45,453 L132.45,451 L130.35,451 C129.7704,451 129.3,450.552 129.3,450 C129.3,449.448 129.7704,449 130.35,449 L132.45,449 L132.45,447 C132.45,446.448 132.9204,446 133.5,446 C134.0796,446 134.55,446.448 134.55,447 L134.55,449 L136.65,449 C137.2296,449 137.7,449.448 137.7,450 M133.5,458 C128.86845,458 125.1,454.411 125.1,450 C125.1,445.589 128.86845,442 133.5,442 C138.13155,442 141.9,445.589 141.9,450 C141.9,454.411 138.13155,458 133.5,458 M133.5,440 C127.70085,440 123,444.477 123,450 C123,455.523 127.70085,460 133.5,460 C139.29915,460 144,455.523 144,450 C144,444.477 139.29915,440 133.5,440" id="plus_circle-[#1427]"> </path> </g> </g> </g> </g>
-</svg>`;
+    plus.innerHTML = plussvg;
 
     newBtn.prepend(plus);
     label.append(filter);
@@ -510,11 +509,7 @@ function createSaveButton(){
     btn.classList.add('btn', 'save-btn');
     btn.type = 'submit';
     const span = document.createElement('span');
-    span.innerHTML = `<svg width="18px" height="18px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g id="SVGRepo_bgCarrier" stroke-width="0"/>
-<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-<g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M18.1716 1C18.702 1 19.2107 1.21071 19.5858 1.58579L22.4142 4.41421C22.7893 4.78929 23 5.29799 23 5.82843V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H18.1716ZM4 3C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21L5 21L5 15C5 13.3431 6.34315 12 8 12L16 12C17.6569 12 19 13.3431 19 15V21H20C20.5523 21 21 20.5523 21 20V6.82843C21 6.29799 20.7893 5.78929 20.4142 5.41421L18.5858 3.58579C18.2107 3.21071 17.702 3 17.1716 3H17V5C17 6.65685 15.6569 8 14 8H10C8.34315 8 7 6.65685 7 5V3H4ZM17 21V15C17 14.4477 16.5523 14 16 14L8 14C7.44772 14 7 14.4477 7 15L7 21L17 21ZM9 3H15V5C15 5.55228 14.5523 6 14 6H10C9.44772 6 9 5.55228 9 5V3Z" fill="currentColor"/> </g>
-</svg>`
+    span.innerHTML = savesvg;
 
     btn.prepend(span);
     return btn;
@@ -827,7 +822,12 @@ function buildTodoModalContent(todo){
     section.classList.add(`${prefix}main`);
     const noteCon = document.createElement('div');
     const noteHead = createBasicElement('h3', 'Notes');
-    noteCon.append(noteHead);
+    const noteBtn = createBasicElement('button', 'New Note');
+    const plus = document.createElement('span');
+    plus.innerHTML = plussvg;
+    noteBtn.prepend(plus);
+    noteBtn.classList.add('new-btn', 'btn');
+    noteCon.append(noteHead, noteBtn);
     todo?.notes?.length > 0 ? todo.notes.forEach(note => { noteCon.appendChild(createBasicElement('p', String(note))) }) : noteCon.appendChild(buildPlaceholder('No Notes'))
 
 
